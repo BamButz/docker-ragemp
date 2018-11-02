63c2a29f5c4d1f9fef3c0e9aecfd62e51e585fad
BamButz <bambutz@cryptonica.de>
2018-11-02 08:52:23 +0100

    Added ci configuration Removed update instruction

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 echo "Copy server binaries"
-cp -u -v /home/server /ragemp/server
-cp -u -v /home/bt.dat /ragemp/bt.dat
+cp -v /home/server /ragemp/server
+cp -v /home/bt.dat /ragemp/bt.dat
 
 echo "Run Server"
 cd /ragemp
