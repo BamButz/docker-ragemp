20968d5630c427e268b073bf5f8b80e3e2bec7c6
BamButz <bambutz@cryptonica.de>
2020-02-25 09:05:41 +0100

    fix: map true/false to enabled/disabled
    resolves #1

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,12 +22,18 @@ sed "s/{MAX_PING}/${RAGEMP_MAX_PING:=120}/g" -i /ragemp/conf.json
 sed "s/{MIN_FPS}/${RAGEMP_MIN_FPS:=24}/g" -i /ragemp/conf.json
 sed "s/{MAX_PACKET_LOSS}/${RAGEMP_MAX_PACKET_LOSS:=0.2}/g" -i /ragemp/conf.json
 sed "s/{ALLOW_CEF_DEBUGGING}/${RAGEMP_ALLOW_CEF_DEBUGGING:=false}/g" -i /ragemp/conf.json
-sed "s/{CSHARP}/${RAGEMP_CSHARP:=disabled}/g" -i /ragemp/conf.json
 sed "s/{ENABLE_HTTP_SECURITY}/${RAGEMP_ENABLE_HTTP_SECURITY:=false}/g" -i /ragemp/conf.json
 sed "s/{VOICE_CHAT}/${RAGEMP_VOICE_CHAT:=false}/g" -i /ragemp/conf.json
 sed "s/{VOICE_CHAT_SAMPLE_RATE}/${RAGEMP_VOICE_CHAT_SAMPLE_RATE:=24000}/g" -i /ragemp/conf.json
 sed "s/{NODE_COMMANDLINE_FLAGS}/$NODE_COMMANDLINE_FLAGS/g" -i /ragemp/conf.json
 
+# Rewrite true/false to enabled/disabled for consistency 
+# to allow all boolean variables to be set with true/false
+if [ "$RAGEMP_CSHARP" = true]; then
+    sed "s/{CSHARP}/${RAGEMP_CSHARP:=enabled}/g" -i /ragemp/conf.json
+else
+    sed "s/{CSHARP}/${RAGEMP_CSHARP:=disabled}/g" -i /ragemp/conf.json
+fi
 
 echo "Run Server"
 cd /ragemp
